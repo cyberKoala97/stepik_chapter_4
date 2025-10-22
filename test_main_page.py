76f65358602538cc806ed4069edbde8614537732
CyberKoala <lya163t@gmail.com>
2025-10-22 21:42:40 +0300

    Add def test_guest_can_go_to_login_page fix

--- test_main_page.py
+++ test_main_page.py
@@ -4,11 +4,6 @@ from selenium.webdriver.common.by import By
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
-
-
-
-      # выполняем метод страницы — переходим на страницу логина
