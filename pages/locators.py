83be8d1482699843f463c97dc410963b878f08cc
CyberKoala <lya163t@gmail.com>
2025-10-26 18:42:41 +0300

    step 8

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,14 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
+class MainPageLocators():
+    pass
 
 class LoginPageLocators():
     LOGIN_URL = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
@@ -14,10 +19,10 @@ class LoginPageLocators():
     REGISTER_PASSWORD_FORM = (By.ID, "id_registration-password1")
     REGISTER_SECOND_PASSWORD_FORM = (By.ID, "id_registration-password2")
 
-class ProductPageLocators:
+
+class ProductPageLocators():
     BTN_ADD_TO_BASKET = (By.CSS_SELECTOR, ".product_main form button.btn-add-to-basket")
     PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main p.price_color")
-    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner strong")
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "#messages div:first-child")
     MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
-    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "")
--- pages/product_page.py
+++ pages/product_page.py
@@ -8,25 +8,27 @@ class ProductPage(BasePage):
         add_button.click()
 
     def should_be_message_about_adding(self):
-        # Сначала проверяем, что элементы присутствуют на странице
         assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), (
             "Product name is not presented")
         assert self.is_element_present(*ProductPageLocators.MESSAGE_ABOUT_ADDING), (
             "Message about adding is not presented")
-        # Затем получаем текст элементов для проверки
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         message = self.browser.find_element(*ProductPageLocators.MESSAGE_ABOUT_ADDING).text
-        # Проверяем, что название товара присутствует в сообщении о добавлении
         assert product_name == message, "No product name in the message"
 
     def should_be_message_basket_total(self):
-        # Сначала проверяем, что элементы присутствуют на странице
         assert self.is_element_present(*ProductPageLocators.MESSAGE_BASKET_TOTAL), (
             "Message basket total is not presented")
         assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), (
             "Product price is not presented")
-        # Затем получаем текст элементов для проверки
         message_basket_total = self.browser.find_element(*ProductPageLocators.MESSAGE_BASKET_TOTAL).text
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
-        # Проверяем, что цена товара присутствует в сообщении со стоимостью корзины
         assert product_price == message_basket_total, "No product price in the message"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.MESSAGE_ABOUT_ADDING), \
+            "Success message is presented, but should not be"
+
+    def should_disappear_success_message(self):
+        assert  self.is_disappeared(*ProductPageLocators.MESSAGE_ABOUT_ADDING), \
+            "Success message is not disappeared"
