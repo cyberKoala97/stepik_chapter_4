20f4065a69029b24d06e87540448543a0fad030b
CyberKoala <lya163t@gmail.com>
2025-10-27 11:14:19 +0300

    Fix test issues: correct browser parameter in test methods and fix login link locator

--- test_main_page.py
+++ test_main_page.py
@@ -7,7 +7,7 @@ from .pages.main_page import MainPage
 
 @pytest.mark.login_guest
 class TestLoginFromMainPage:
-    def test_guest_can_go_to_login_page(browser):
+    def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
         page.open()  # открываем страницу
@@ -15,7 +15,7 @@ class TestLoginFromMainPage:
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
 
-    def test_guest_should_see_login_link(browser):
+    def test_guest_should_see_login_link(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
